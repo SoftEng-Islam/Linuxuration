1f27eef22a347bb8f480765c588a2b27c36e771b
Islam Ahmed <softeng.islam@gmail.com>
2024-07-07 02:58:54 +0300

    update

--- scripts/wifitools.sh
+++ scripts/wifitools.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # This script can install hashcat & hcxdumptool & hcxtools
+
+
+cd ~/Downloads/
+git clone https://github.com/ZerBea/hcxdumptool.git
+cd hcxdumptool
+make -j $(nproc)
+sudo make install
