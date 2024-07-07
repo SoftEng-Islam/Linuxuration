d581bc9487858531ac4db9be3d6730df93d6799c
Islam Ahmed <softeng.islam@gmail.com>
2024-07-08 02:21:42 +0300

    rebuild

---
+++ scripts/Both/developer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# =============================================================
+# Just script to install some Programming Languages
+# and Frameworks, Tools that related to Web Development Stack.
+# =============================================================
+
+
+# NodeJS
+
+
+
+# PHP
