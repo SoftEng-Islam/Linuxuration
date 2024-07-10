c0562f5041b30f473a8e0d74091e720a8e18a5be
Islam Ahmed <softeng.islam@gmail.com>
2024-07-10 13:56:29 +0300

    add cursor Icons

---
+++ scripts/Both/cursor.sh
@@ -0,0 +1,31 @@
+# !/bin/bash
+# =====================================
+# Install Cursor Icons
+# =====================================
+
+echo 'Install Cursor Icons'
+
+# ----------------------------------------------------
+# Bibata Rainbow
+# --------------
+# Description:
+# Bibata Cursor Theme with semi-animated rainbow colours. This Cursor is built with clickgen and render with the puppeteer.
+# ----------------------------------------------------
+# Github:       https://github.com/ful1e5/Bibata_Cursor_Rainbow
+# gnome-look:   https://www.gnome-look.org/p/1445634
+# ----------------------------------------------------
+
+# Install
+# For all user:
+# sudo mv Bibata-Rainbow-* /usr/share/icons
+
+# For local user:
+# mv Bibata-Rainbow-* ~/.icons
+
+
+# Uninstall
+# From all user:
+# sudo rm -r /usr/share/icons/Bibata-Rainbow-*
+
+# From local user:
+# rm -r ~/.icons/Bibata-Rainbow-*
