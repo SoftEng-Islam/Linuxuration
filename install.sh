9691e12896746c8803eb7dac8f2d91ff5061684a
Islam Ahmed <softeng.islam@gmail.com>
2024-07-10 22:11:27 +0300

    just update

--- install.sh
+++ install.sh
@@ -7,36 +7,60 @@
 # Please don't use this script, still under development
 # =======================================================
 
-
-
 # ----------------------------------------------------
 # Check if running as root. If root, script will exit
 # ----------------------------------------------------
 if [[ $EUID -eq 0 ]]; then
-    echo "This script should not be executed as root! Exiting......."
-    exit 1
+	echo "This script should not be executed as root! Exiting......."
+	exit 1
 fi
 clear
 
-
 # ------------------------------------
-#
+# set colors into vars
 # ------------------------------------
-trap ctrl_c INT
 
-RED='\033[0;31m'
-LBLUE='\033[1;34m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+LBLUE='\033[34m'
 ORANGE='\033[0;33m'
+
+# Regular Colors:
+RBlack='\033[30m'
+RRed='\033[31m'
+RGreen='\033[32m'
+RYellow='\033[33m'
+RBlue='\033[34m'
+RMagenta='\033[35m'
+RCyan='\033[36m'
+RWhite='\033[37m'
+
+# Bright (Bold) Colors:
+BrBlack='\033[90m'
+BrRed='\033[91m'
+BrGreen='\033[92m'
+BrYellow='\033[93m'
+BrBlue='\033[94m'
+BrMagenta='\033[95m'
+BrCyan='\033[96m'
+BrWhite='\033[97m'
+
 NC='\033[0m'
+BOLD='\033[1m'
+Italic='\033[3m'
+Underline='\033[4m'
+Strikethrough='\033[9m'
 
-
+# Unicode Characters
+# `echo -e "\u2764"   # Outputs a heart symbol (❤)`
+heart='\u2764'
 # ---------------------------------------------------------------
 # Say Hello, and give the user some information about his device
 # ---------------------------------------------------------------
-echo -e "$(cat <<EOF
-Welcome \e[1;32m$(whoami)\e[0m
+echo -e "$(
+	cat <<EOF
+Welcome \e[1;32m$(whoami)\e[0m \e[95m\u2764\e[0m
 Today's date is: \e[93m$(date)\e[0m \n
 feel free to message me on Twitter: https://x.com/SoftEng_Islam
 
@@ -63,7 +87,6 @@ EOF
 # Storage /
 # Storage /home
 
-
 # -----------------------------------------------
 # Options for User
 # -----------------------------------------------
@@ -79,7 +102,6 @@ echo 'Select your Options:'
 # Install ZSH & OH-MY-ZSH
 # Install Pomodoro
 # Install plymouth-themes
-
 
 # ---------------------
 # Update the Packages
@@ -107,8 +129,6 @@ makepkg -si
 # echo 'Initialize `debtap` (only needed the first time)'
 # debtap -u
 
-
-
 # Microsoft Partition FileSystem Format 'NTFS'
 echo 'Install ntfs-3g to Support MS partition file system'
 pacman -Sy --noconfirm ntfs-3g
@@ -118,7 +138,6 @@ modprobe fuse
 sudo pacman -S --noconfirm zsh fzf
 chsh -s /bin/zsh
 
-
 # Kitty terminal emulator
 echo 'Install Kitty terminal emulator'
 sudo pacman -S --noconfirm kitty
@@ -126,10 +145,6 @@ echo 'Set Kitty as the Default Terminal:'
 export TERMINAL=kitty
 # Kitty's configuration file can be found at ~/.config/kitty/kitty.conf.
 # You can customize Kitty's appearance and behavior by editing this file.
-
-
-
-
 
 # ######################
 # install-OneUI
@@ -144,35 +159,23 @@ export TERMINAL=kitty
 #########################
 yay -S gnome-pomodoro
 
-
-
-
-
-
 echo 'Disable Gnome Check-alive'
 gsettings set org.gnome.mutter check-alive-timeout 0
-
-
-
 
 pacman -S flatpak --noconfirm
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # increase the timeout settings for Flatpak
 FLATPAK_CONF="/etc/flatpak/flatpak.conf"
-echo "[Network]" >> $FLATPAK_CONF
+echo "[Network]"  >> $FLATPAK_CONF
 # Increase timeout settings (adjust as needed)
-echo "RequestTimeout=800" >> $FLATPAK_CONF
+echo "RequestTimeout=800"  >> $FLATPAK_CONF
 echo "Timeout settings updated in $FLATPAK_CONF"
-
-
 
 #######################
 # overclocing
 #######################
 yay -S corectrl
-
-
 
 ############
 # plymouth
@@ -185,43 +188,21 @@ systemctl status plymouth.service
 ls /usr/share/plymouth/themes/
 sudo plymouth-set-default-theme -R details
 
-
-
-
-
 ##########################
 # Install ZSH & oh_my_zsh
 ##########################
 
-
-
-
-
 ################################
 # Install Some Gnome Extensions
 ################################
-
-
-
-
-
-
-
-
-
-
-
-
 
 ##########################
 # Install Apps
 # `flatpak install -y flathub com.visualstudio.code org.mozilla.firefox`
 ##########################
 echo 'Installing Resources App'
-flatpak install flathub com.visualstudio.code  org.mozilla.firefox
+flatpak install flathub com.visualstudio.code org.mozilla.firefox
 net.nokyan.Resources com.microsoft.Edge com.google.Chrome com.mattjakeman.ExtensionManager org.qbittorrent.qBittorrent com.protonvpn.www com.github.zadam.trilium
-
-
 
 # List of Flatpak applications to install (replace with your desired apps)
 APPS=(
@@ -248,11 +229,11 @@ APPS=(
 	"org.blender.Blender"
 	"com.obsproject.Studio"
 	"org.gnome.Chess"
-    # Beekeeper Studio
-    # insomnia
-    # Lunacy - UI/UX and Web Designer Tool
-    # Krita
-    # blender
+	# Beekeeper Studio
+	# insomnia
+	# Lunacy - UI/UX and Web Designer Tool
+	# Krita
+	# blender
 	# Add more applications as needed
 )
 # Function to install Flatpak applications
@@ -262,7 +243,7 @@ install_flatpak_apps() {
 		flatpak install flathub $app -y
 		echo "---------------------------------------------"
 	done
-	}
+}
 # Main execution
 install_flatpak_apps
 echo "All Flatpak applications installed successfully."
