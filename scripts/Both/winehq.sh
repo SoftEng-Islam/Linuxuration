0785fe5673f2e9f03746b0078245b6e17e62d957
Islam Ahmed <softeng.islam@gmail.com>
2024-07-11 11:17:07 +0300

    add Trizen

--- scripts/Both/winehq.sh
+++ scripts/Both/winehq.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-# -----------------
+# ===============================
 # Install wineHQ
-# -----------------
+# ===============================
+
+# Install Wine and some essential dependencies:
+sudo pacman -S --noconfirm wine winetricks wine-mono wine-gecko
+
+# Once installed, you can configure Wine by running the Wine configuration tool:
+winecfg
+
+# Verify the Installation
+wine --version
+
+# PlayOnLinux: A graphical...
+#..front-end for Wine that simplifies the installation and management of Windows applications:
+# Install PlayOnLinux from AUR
+yay -S --noconfirm playonlinux
+
+# After installation, you can run PlayOnLinux by executing the following command:
+playonlinux
+
+# Examples:
+# Running Windows Applications
+# wine /path/to/your/application.exe
+
+# Installing Notepad++
+# wine npp.7.9.5.Installer.exe
+
+# Troubleshooting
+# If you encounter issues, here are some tips:
+# Winetricks: Use winetricks to install missing libraries or components:
+# winetricks
