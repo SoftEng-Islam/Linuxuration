057c8fb779ab37e9990a01a370ba3d880846805d
Islam Ahmed <softeng.islam@gmail.com>
2024-07-19 15:07:00 +0300

    update Networks File

--- configs/networks.sh
+++ configs/networks.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
+# ---------------------------------------------- #
+# Here some fixies for Network & WIFI & Internet #
+# ---------------------------------------------- #
+# Features:
+#
+# ---------------------------------------------- #
 
-#############################
-# Install firewalld
-#############################
-sudo pacman -S firewalld networkmanager wpa_supplicant dhclient iw wireless_tools dialog iptables dnsmasq hostapd nm-connection-editor connman dhcpcd network-manager-applet netctl
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
+# ----------------- #
+# Update The System #
+# ----------------- #
+sudo pacman --noconfirm -Syu
+sudo pacman --noconfirm -S linux-firmware
 
-sudo tee /etc/NetworkManager/conf.d/00-local.conf << EOF > /dev/null
+# -------------------------- #
+# Install Important Packages #
+# -------------------------- #
+sudo pacman -S --noconfirm \
+	firewalld networkmanager \
+	wpa_supplicant dhclient \
+	iw wireless_tools \
+	dialog iptables dnsmasq \
+	hostapd nm-connection-editor \
+	connman dhcpcd network-manager-applet netctl
+
+# Configure firewall
+sudo tee /etc/NetworkManager/conf.d/00-local.conf <<EOF >/dev/null
 [main]
 firewall-backend=none
 EOF
 sudo systemctl restart NetworkManager.service
-
 
 ### Check Existing Policies
 sudo firewall-cmd --permanent --get-policies
@@ -34,58 +50,60 @@ sudo firewall-cmd --permanent --policy=ingress-shared --add-ingress-zone=trusted
 sudo firewall-cmd --permanent --policy=ingress-shared --add-egress-zone=nm-shared
 sudo firewall-cmd --reload
 
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan0 -o eno1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eno1 -o wlan0 -j ACCEPT
 
-#
+# Enable IP Forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
+# ----------------------------------------
+sudo -i
+# check the status of the service (running and enabled)
+systemctl status firewalld
+# if the service is not running, start it
+systemctl start firewalld
+# if the service has exited, restart it(check for error if any)
+systemctl restart firewalld
+# if the service is not enabled, enable it
+systemctl enable firewalld
 
 # check the status of the service (running and enabled)
-sudo systemctl status firewalld
+systemctl status dnsmasq
 # if the service is not running, start it
-sudo systemctl start firewalld
+systemctl start dnsmasq
 # if the service has exited, restart it(check for error if any)
-sudo systemctl restart firewalld
+systemctl restart dnsmasq
 # if the service is not enabled, enable it
-sudo systemctl enable firewalld
+systemctl enable dnsmasq
 
 # check the status of the service (running and enabled)
-sudo systemctl status dnsmasq
+systemctl status NetworkManager
 # if the service is not running, start it
-sudo systemctl start dnsmasq
+systemctl start NetworkManager
 # if the service has exited, restart it(check for error if any)
-sudo systemctl restart dnsmasq
+systemctl restart NetworkManager
 # if the service is not enabled, enable it
-sudo systemctl enable dnsmasq
+systemctl enable NetworkManager
 
 # check the status of the service (running and enabled)
-sudo systemctl status NetworkManager
+systemctl status systemd-resolved
 # if the service is not running, start it
-sudo systemctl start NetworkManager
+systemctl start systemd-resolved
 # if the service has exited, restart it(check for error if any)
-sudo systemctl restart NetworkManager
+systemctl restart systemd-resolved
 # if the service is not enabled, enable it
-sudo systemctl enable NetworkManager
+systemctl enable systemd-resolved
 
-# check the status of the service (running and enabled)
-sudo systemctl status systemd-resolved
-# if the service is not running, start it
-sudo systemctl start systemd-resolved
-# if the service has exited, restart it(check for error if any)
-sudo systemctl restart systemd-resolved
-# if the service is not enabled, enable it
-sudo systemctl enable systemd-resolved
-
-
+exit
+# --------------------------------
 
 # Here are some recommendations To improve and enhance Wi-Fi and internet speed:
 # This disables Wi-Fi power-saving mode, which can help improve performance.
-sudo tee /etc/NetworkManager/NetworkManager.conf << EOF >> /dev/null
+sudo tee /etc/NetworkManager/NetworkManager.conf <<EOF >>/dev/null
 [connection]
 wifi.powersave = 2
 EOF
-
-
-
 
 # Disable IPv6
 # sudo tee /etc/sysctl.d/ipv6.conf << EOF > /dev/null
