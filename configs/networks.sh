8e1bf770c4d673ca0a7e2a453461b20f33c2316c
Islam Ahmed <softeng.islam@gmail.com>
2024-07-20 13:22:51 +0300

    update & Improve networks.sh file.

--- configs/networks.sh
+++ configs/networks.sh
@@ -46,7 +46,19 @@ sudo tee /etc/NetworkManager/conf.d/00-local.conf <<EOF >/dev/null
 [main]
 firewall-backend=none
 EOF
-sudo systemctl restart NetworkManager.service
+
+# Here are some recommendations To improve and enhance Wi-Fi and internet speed:
+# This disables Wi-Fi power-saving mode, which can help improve performance.
+sudo tee /etc/NetworkManager/NetworkManager.conf <<EOF >>/dev/null
+[connection]
+wifi.powersave = 2
+EOF
+
+# Disable IPv6
+sudo tee /etc/sysctl.d/ipv6.conf <<EOF >/dev/null
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.eth0.disable_ipv6 = 1
+EOF
 
 ### Check Existing Policies
 sudo firewall-cmd --permanent --get-policies
@@ -73,65 +85,45 @@ sudo firewall-cmd --reload
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo iptables -A FORWARD -i wlan0 -o eno1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i eno1 -o wlan0 -j ACCEPT
-
 # Save iptables Rules
 # To ensure the iptables rules persist after a reboot, save them using `iptables-save`:
 sudo iptables-save >/etc/iptables/iptables.rules
-sudo systemctl enable iptables
 
 # Enable IP Forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
 
-# ----------------------------------------
+# --------------- #
+# Enable Services #
+# --------------- #
 sudo -i
 # check the status of the service (running and enabled)
-systemctl status firewalld
+systemctl status iptables
 # if the service is not running, start it
-systemctl start firewalld
+systemctl start iptables
 # if the service has exited, restart it(check for error if any)
-systemctl restart firewalld
+systemctl restart iptables
 # if the service is not enabled, enable it
+systemctl enable iptables
+
+systemctl status firewalld
+systemctl start firewalld
+systemctl restart firewalld
 systemctl enable firewalld
 
-# check the status of the service (running and enabled)
 systemctl status dnsmasq
-# if the service is not running, start it
 systemctl start dnsmasq
-# if the service has exited, restart it(check for error if any)
 systemctl restart dnsmasq
-# if the service is not enabled, enable it
 systemctl enable dnsmasq
 
-# check the status of the service (running and enabled)
 systemctl status NetworkManager
-# if the service is not running, start it
 systemctl start NetworkManager
-# if the service has exited, restart it(check for error if any)
 systemctl restart NetworkManager
-# if the service is not enabled, enable it
 systemctl enable NetworkManager
 
-# check the status of the service (running and enabled)
 systemctl status systemd-resolved
-# if the service is not running, start it
 systemctl start systemd-resolved
-# if the service has exited, restart it(check for error if any)
 systemctl restart systemd-resolved
-# if the service is not enabled, enable it
 systemctl enable systemd-resolved
 
 exit
 # --------------------------------
-
-# Here are some recommendations To improve and enhance Wi-Fi and internet speed:
-# This disables Wi-Fi power-saving mode, which can help improve performance.
-sudo tee /etc/NetworkManager/NetworkManager.conf <<EOF >>/dev/null
-[connection]
-wifi.powersave = 2
-EOF
-
-# Disable IPv6
-# sudo tee /etc/sysctl.d/ipv6.conf << EOF > /dev/null
-# net.ipv6.conf.all.disable_ipv6 = 1
-# net.ipv6.conf.eth0.disable_ipv6 = 1
-# EOF
