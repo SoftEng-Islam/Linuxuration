aaa5fc27ac6de50d8f4fa1ed9bed6d3f2f93e11d
Islam Ahmed <softeng.islam@gmail.com>
2024-09-14 21:06:55 +0300

    update

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,12 +1,10 @@
-import { Bar } from "./simple-bar/bar.js";
-import { applauncher } from "./applauncher/applauncher.js";
-import { NotificationPopups } from "./notification-popups/notificationPopups.js";
+import { Bar } from "./modules/simple-bar/bar.js";
+import { applauncher } from "./modules/applauncher/applauncher.js";
+import { NotificationPopups } from "./modules/notification-popups/notificationPopups.js";
 App.config({
 	style: "./style.css",
 	windows: [
-		// @ts-ignore
 		Bar(),
-		// @ts-ignore
 		NotificationPopups(),
 		applauncher,
 	],
