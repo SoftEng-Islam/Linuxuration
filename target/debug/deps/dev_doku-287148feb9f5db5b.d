3b9d6668415132f8a30d60c4a988687990bdf687
Islam Ahmed <softeng.islam@gmail.com>
2024-10-14 18:38:37 +0300

    we working...

---
+++ target/debug/deps/dev_doku-287148feb9f5db5b.d
@@ -0,0 +1,5 @@
+/data/current/confArch-SH/target/debug/deps/libdev_doku-287148feb9f5db5b.rmeta: src/main.rs
+
+/data/current/confArch-SH/target/debug/deps/dev_doku-287148feb9f5db5b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/dev_doku-950a7e5029adc34b.d
@@ -0,0 +1,5 @@
+/data/current/confArch-SH/target/debug/deps/libdev_doku-950a7e5029adc34b.rmeta: src/main.rs
+
+/data/current/confArch-SH/target/debug/deps/dev_doku-950a7e5029adc34b.d: src/main.rs
+
+src/main.rs:
