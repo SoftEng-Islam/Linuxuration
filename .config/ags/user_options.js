c1b40ecd4fce45e9e89d2f6fad080256b7b467a0
Islam Ahmed <softeng.islam@gmail.com>
2024-10-15 19:04:23 +0300

    we working...

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -1,11 +1,11 @@
 // For every option, see ~/.config/ags/modules/.configuration/user_options.js
 // (vscode users ctrl+click this: file://./modules/.configuration/user_options.js)
 // (vim users: `:vsp` to split window, move cursor to this path, press `gf`. `Ctrl-w` twice to switch between)
-//   options listed in this file will override the default ones in the above file
-
+// options listed in this file will override the default ones in the above file
 const userConfigOptions = {
 	appearance: {
 		keyboardUseFlag: true,
+		barRoundCorners: 0,
 		fakeScreenRounding: 0
 	},
 	time: {
