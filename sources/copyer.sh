82892748ff828f256ab4104e38c93cb7a8ebff64
Islam Ahmed <softeng.islam@gmail.com>
2024-11-05 00:14:22 +0200

    update

--- sources/copyer.sh
+++ sources/copyer.sh
@@ -5,21 +5,24 @@ copy_files() {
 	
   # "$HOME"/dotfiles/setup/copy.sh
   echo ":: gtk..."
+
   mv "$HOME"/.config/gtk-3.0 --backup "$HOME"/.config/gtk-3.0-bk
   mv "$HOME"/.config/gtk-4.0 --backup "$HOME"/.config/gtk-4.0-bk
+  
   cp -r -f "$HOME"/dotfiles/setup/gtk-3.0 "$HOME"/.config/
   cp -r -f "$HOME"/dotfiles/setup/gtk-4.0 "$HOME"/.config/
+  
   echo ":: wl-gammarelay..."
   mkdir -p "$HOME"/.config/systemd/user/
   cp "$HOME"/dotfiles/setup/wl-gammarelay.service "$HOME"/.config/systemd/user/
   systemctl --user daemon-reload
   systemctl --user enable --now wl-gammarelay.service
+
   echo ":: Product Sans font"
   sudo cp -r "$HOME"/dotfiles/setup/google-sans /usr/share/fonts
   sudo fc-cache -f -v
+
   echo ":: Done!"
-
-
 
 
 	if [ -d "$HOME/wallpaper" ]; then
@@ -30,25 +33,21 @@ copy_files() {
 }
 create_links() {
 	echo ":: Creating links"
-	# ln -f "$HOME"/dotfiles/electron-flags.conf "$HOME"/.config/electron-flags.conf
-	# ln -s "$HOME"/dotfiles/ags "$HOME"/.config/ags
-	# ln -s "$HOME"/dotfiles/alacritty "$HOME"/.config/alacritty
-	# ln -s "$HOME"/dotfiles/hypr "$HOME"/.config/hypr
-	ln -s /data/current/Linuxuration/.config/swappy ~/.config/swappy
+	ln -f /data/current/Linuxuration/.config/electron-flags.conf ~/.config/electron-flags.conf
 	ln -s /data/current/Linuxuration/.config/ags ~/.config/ags
-	ln -s /data/current/Linuxuration/.config/hypr ~/.config/hypr
-	ln -s /data/current/Linuxuration/.config/kitty ~/.config/kitty
+	ln -s /data/current/Linuxuration/.config/code-flags.conf ~/.config/code-flags.conf
 	ln -s /data/current/Linuxuration/.config/gtk-2.0 ~/.config/gtk-2.0
 	ln -s /data/current/Linuxuration/.config/gtk-3.0 ~/.config/gtk-3.0
 	ln -s /data/current/Linuxuration/.config/gtk-4.0 ~/.config/gtk-4.0
+	ln -s /data/current/Linuxuration/.config/hypr ~/.config/hypr
+	ln -s /data/current/Linuxuration/.config/kitty ~/.config/kitty
+	ln -s /data/current/Linuxuration/.config/msedge-flags.conf ~/.config/msedge-flags.conf
+	ln -s /data/current/Linuxuration/.config/psd ~/.config/psd
 	ln -s /data/current/Linuxuration/.config/qt5ct ~/.config/qt5ct
 	ln -s /data/current/Linuxuration/.config/qt6ct ~/.config/qt6ct
-	ln -s /data/current/Linuxuration/.config/psd ~/.config/psd
-	ln -s /data/current/Linuxuration/.config/electron-flags.conf ~/.config/electron-flags.conf
-	ln -s /data/current/Linuxuration/.config/msedge-flags.conf ~/.config/msedge-flags.conf
-	ln -s /data/current/Linuxuration/.config/code-flags.conf ~/.config/code-flags.conf
+	ln -s /data/current/Linuxuration/.config/scripts ~/.config/scripts
+	ln -s /data/current/Linuxuration/.config/swappy ~/.config/swappy
 	ln -s /data/current/Linuxuration/home/.zshrc ~/.zshrc
 	ln -s /data/current/Linuxuration/home/wallpapers ~/wallpapers
   ls -s /data/current/Linuxuration/home/mumble-dark.qbtheme ~/mumble-dark.qbtheme
-	ln -s /data/current/Linuxuration/.config/scripts ~/.config/scripts
 }
