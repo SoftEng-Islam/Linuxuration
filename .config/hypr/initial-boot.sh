557980527d1505c3532d8b063ecdfb77f7062ab0
Islam Ahmed <softeng.islam@gmail.com>
2024-11-09 02:56:37 +0200

    update

--- .config/hypr/initial-boot.sh
+++ .config/hypr/initial-boot.sh
@@ -10,7 +10,7 @@ scriptsDir=$HOME/.config/hypr/scripts
 wallpaper=$HOME/.config/hypr/wallpaper_effects/.wallpaper_modified
 # kvantum_theme="Catppuccin-Mocha"
 color_scheme="prefer-dark"
-gtk_theme="Default"
+gtk_theme="Fluent-Dark"
 icon_theme="Papirus-Dark"
 cursor_theme="Bibata-Modern-Ice"
 
